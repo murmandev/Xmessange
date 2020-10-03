c0238e9ff60362eca58f38b32b4c8c83bac22d9c
EternalTuna <51095870+EternalTuna@users.noreply.github.com>
2020-10-03 17:23:41 +0300

    Py-server on Flask lib
    Fixed bugs - invalid syntax

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import time 
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
@@ -10,14 +10,36 @@ def hello():
     return "Hello, World! <br><a href='/status'>Статус</a>"
 
 @app.route("/status")
-def hello():
+def status():
     return {
         'status': True,
         'name': 'Xmessage Messanger',
-        'time': time.time()
-        'time2': time.asctime()
+        'time': time.time(),
+        'time2': time.asctime(),
         'time3': datetime.now()
     }
+
+@app.route("/send_message", methods=['POST'])
+def send_message():
+    data = request.json
+    print(data)
+    text = data['text']
+    author = data['author']
+
+    if isinstance(text, str) and isinstance(author, str):
+        db.append({
+            'text': text,
+            'author': author,
+            'time': time.time()
+        })
+        return Response({'ok': True})
+    else:
+        return Response({'ok': False}, 400)
+
+
+@app.route("/get_messages")
+def get_messages():
+    return {'messages': db}
 
 
 app.run()
